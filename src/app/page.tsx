a52d37b998b50381159ba7fd611c722db8c163d6
Muhammad Arslan <114974765+marslanmustafa@users.noreply.github.com>
2024-06-27 12:15:32 +0500

    home done except last div(faq)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,81 @@
+import ServiceCard from "@/components/ServiceCard";
+import { Service } from "@/lib/types";
 import Image from "next/image";
+import Link from "next/link";
+
+const ServicesCards: Service[] = [
+  {
+    icon: "/images/home/servicesCards/Transportation.svg",
+    heading: "Transportation Services",
+    para: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    link: "/services/Transportation",
+  },
+  {
+    icon: "/images/home/servicesCards/LocalCourier.svg",
+    heading: "Local Courier Service",
+    para: "We offer a fast and reliable courier service for the community. Whether it's important documents ",
+    link: "/services/LocalCourier",
+  },
+  {
+    icon: "/images/home/servicesCards/LastMileDelivery.svg",
+    heading: "Last Mile Delivery Services",
+    para: "In partnership with local businesses, our motorcycle rickshaw service ensures that goods reach their....",
+    link: "/services/LastMile",
+  },
+  {
+    icon: "/images/home/servicesCards/adModel.svg",
+    heading: "Advertisement Model for Rickshaw Services",
+    para: "We offer advertising space on our rickshaws to local businesses. Advertisements can be full or partial wraps",
+    link: "/services/adModel",
+  },
+];
 
 const Home = () => {
   return (
     <div className="overflow-x-hidden w-full">
-
-
-      {/* div3 */}
-      <div className="relative bg-foreground flex items-center w-full h-fit md:h-[160px] xl:min-h-[200px] px-4 md:px-8 lg:px-20 xl:px-32 py-5">
-          <p className="w-full md:w-[82%] lg:w-[86%] text-xs sm:text-sm md:text-lg lg:text-2xl xl:text-3xl tracking-wide text-white font-semibold">
-          <span className="!text-themeGreen">We believe</span> in the power of partnership and the potential of every individual, regardless of their physical abilities. 
-          </p>
-          <div className="w-[312px] h-[80px] xl:w-[384px] xl:h-[98px] hidden md:block md:absolute rotate-12 bottom-14 px-4 md:right-8 lg:right-20 xl:right-32">
-            <Image
-              src="/images/DoodleLine.svg"
-              alt=""
-              width={386}
-              height={106}
-              className="h-auto w-auto"
-            />
+      {/* div1 */}
+      <div
+        style={{
+          background: `url(/images/home/LandingBg.svg)`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className="relative bg-foreground flex items-center w-[768px] h-[386px] sm:w-full sm:h-[336px] md:h-[436px] lg:h-[596px] xl:h-[696px] px-4 md:px-8 lg:px-20 xl:px-32 py-5"
+      >
+        <div className="px-4 h-full w-screen sm:w-[60%] bg-transparent flex flex-col justify-center gap-10 xl:gap-16">
+          <h1 className="text-2xl md:text-4xl xl:text-6xl text-white font-bold xl:leading-[80px]">
+            Empowering individuals with <span className="text-themeGreen">
+            disabilities.
+            </span>
+          </h1>
+          <h2 className="text-lg md:text-2xl xl:text-4xl text-white">
+            Through self-reliance and collaboration.
+          </h2>
+          <div className={"w-fit px-4 py-2 lg:px-8 lg:py-3 text-sm lg:text-textEm md:font-medium lg:font-bold border border-themeGreen rounded-lg text-themeGrayText bg-themeGreen"
+            }>
+          <Link href="/hire">Hire MeriRide</Link>
           </div>
-        
+        </div>
       </div>
-
-      {/* div4 */}
+      {/* div2 */}
+      <div className="relative bg-foreground flex items-center w-full h-fit md:h-[160px] xl:min-h-[200px] px-4 md:px-8 lg:px-20 xl:px-32 py-5">
+        <p className="w-full md:w-[82%] lg:w-[86%] text-sm md:text-lg lg:text-2xl xl:text-3xl tracking-wide text-white font-semibold">
+          <span className="!text-themeGreen">We believe</span> in the power of
+          partnership and the potential of every individual, regardless of their
+          physical abilities.
+        </p>
+        <div className="w-[312px] h-[80px] xl:w-[384px] xl:h-[98px] hidden md:block md:absolute bottom-14 px-4 md:right-8 lg:right-20 xl:right-32">
+          <Image
+            src="/images/DoodleLine.svg"
+            alt=""
+            width={386}
+            height={106}
+            className="h-auto w-auto"
+          />
+        </div>
+      </div>
+      {/* div3 */}
       <div className="border-t border-b border-border w-full h-fit flex items-center justify-between gap-5 md:pb-24 xl:min-h-screen px-4 py-8 md:px-8 lg:px-20 xl:px-32 md:pt-12">
         <div className="hidden md:block md:w-1/2">
           <Image
@@ -34,25 +87,25 @@ const Home = () => {
           />
         </div>
         <div className="w-full md:w-1/2 flex justify-start lg:gap-6 flex-col text-foreground">
-          <h1 className="text-2xl lg:text-4xl xl:text-5xl font-bold pb-1 lg-pb-0">
+          <h1 className="text-3xl lg:text-4xl xl:text-5xl font-bold pb-1 lg-pb-0">
             About Us
           </h1>
-          <h2 className="text-textEm md:text-lg lg:text-xl xl:text-2xl pb-5 lg-pb-0">
+          <h2 className="text-lg lg:text-xl xl:text-2xl pb-5 lg-pb-0">
             Welcome to Empowerment
           </h2>
-          <p className="text-[10px] md:text-xs lg:text-sm xl:text-textEm tracking-wide pb-3 lg-pb-0">
+          <p className="text-xs lg:text-sm xl:text-textEm tracking-wide pb-3 lg-pb-0">
             Through Partnership, a{" "}
             <span className="font-semibold"> non-profit organization</span>{" "}
             dedicated to transforming the lives of individuals with disabilities
             by creating sustainable income opportunities
           </p>
-          <p className="text-[10px] md:text-xs lg:text-sm xl:text-textEm tracking-wide pb-3 lg-pb-0">
+          <p className="text-xs lg:text-sm xl:text-textEm tracking-wide pb-3 lg-pb-0">
             Our mission is to bridge the gap between able-bodied{" "}
             <span className="font-semibold">individuals and those</span> with
             disabilities, especially the blind, to foster a collaborative
             environment where both can thrive.
           </p>
-          <p className="text-[10px] md:text-xs lg:text-sm xl:text-textEm tracking-wide">
+          <p className="text-xs lg:text-sm xl:text-textEm tracking-wide">
             <span className="font-semibold">We believe</span> in the power of
             partnership and the potential of every individual, regardless of
             their physical abilities. By developing innovative projects that
@@ -62,8 +115,23 @@ const Home = () => {
           </p>
         </div>
       </div>
-      {/* div5 */}
+      {/* div4 */}
       <div className="w-full h-fit md:pb-24 px-4 py-8 md:px-8 lg:px-20 xl:px-32 md:pt-12">
+        <h1 className="text-2xl xl:text-4xl font-semibold pb-10">
+          Our Services
+        </h1>
+        <div className="w-full text-themeGrayText grid grid-cols-1 sm:grid-cols-2 justify-items-center xl:flex xl:items-center xl:justify-start gap-5">
+          {ServicesCards.map((service, index) => (
+            <ServiceCard
+              key={index}
+              icon={service.icon}
+              heading={service.heading}
+              para={service.para}
+              link={service.link}
+            />
+          ))}
+        </div>
+        {/* div5 joinus */}
         <div className="mt-4 sm:mt-10 md:mt-20 w-full h-fit md:h-[248px] lg:h-[256px] xl:h-[305px] items-center rounded-3xl text-white bg-foreground flex justify-between gap-3 overflow-hidden">
           <div className=" w-full md:w-[calc(100%-352px)] lg:w-[calc(100%-432px)] p-5 md:py-10 md:pl-10 md:pr-2 xl:pl-12 xl:pr-0 xl:py-12 space-y-3 xl:space-y-6">
             <h1 className="text-lg sm:text-xl md:text-[22px] lg:text-[26px] xl:text-[34px] font-semibold">
---
+++ src/lib/types.ts
@@ -0,0 +1,8 @@
+// types.ts
+
+export type Service = {
+  icon: string;
+  heading: string;
+  para: string;
+  link: string;
+}
